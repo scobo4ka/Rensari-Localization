2116ec963d0a4bc582b81905efebf5a82f8db3ab
sadtweenk <k.m.evmenenko@ruc.su>
2025-03-11 13:28:01 +0300

    [init] Инициализация проекта

---
+++ locales/en.ts
@@ -0,0 +1,7 @@
+export default {
+    header_navigation: {
+        schedule: 'Schedule',
+        library: 'Library',
+        catalog: 'Catalog'
+    },
+}
---
+++ locales/ru.ts
@@ -0,0 +1,7 @@
+export default {
+    header_navigation: {
+        schedule: 'Расписание',
+        library: 'Библиотека',
+        catalog: 'Каталог'
+    },
+}
