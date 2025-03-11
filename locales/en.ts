1dbe9ac5d825b997eeeebfb9ce4614949ac68129
sadtweenk <k.m.evmenenko@ruc.su>
2025-03-11 13:32:13 +0300

    [replace] ts файлы на json

--- locales/en.ts
+++
@@ -1,7 +0,0 @@
-export default {
-    header_navigation: {
-        schedule: 'Schedule',
-        library: 'Library',
-        catalog: 'Catalog'
-    },
-}
--- locales/ru.ts
+++
@@ -1,7 +0,0 @@
-export default {
-    header_navigation: {
-        schedule: 'Расписание',
-        library: 'Библиотека',
-        catalog: 'Каталог'
-    },
-}
